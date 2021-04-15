a6f392a0419c190c1bc2031acf395cbcdeaecf91
Rose Pfeiffer <rpfeiffe@uvm.edu>
2021-04-14 20:06:18 -0400

    Added adjustments to figure and removed unnecessary packages.

--- Week162021.R
+++ Week162021.R
@@ -1,7 +1,5 @@
 require(tidyverse)
 require(tidytuesdayR)
-require(maps)
-require(mapproj)
 require(usmap)
 tuesdata <- tidytuesdayR::tt_load('2021-04-13')
 postoff <- tuesdata$post_offices
@@ -12,18 +10,17 @@ openpo <- postoff %>%
 openpo$state <- factor(openpo$state)
 
 openpo <- openpo[-c(8,24,48),]
-  
 
-states <- map_data("state")
-
-
-plot_usmap(,data = openpo, values = "no_rows", color = "black", labels=FALSE)+
+plot <- plot_usmap(,data = openpo, values = "no_rows", color = "black", labels=FALSE)+
   scale_fill_continuous(low = "#3662DB", high = "#E02F2F",
                         name = "Total Number of Post Offices", 
                         label = scales::comma) +
   theme(legend.position = "right") +
-  theme(panel.background = element_rect(colour = "black")) +
-  labs(title = "Historical Post Office Distribution in the US") 
-  
+  theme(panel.background = element_rect(colour = "black"),
+        plot.caption = element_text(hjust = 2.1)) +
+  labs(title = "Historical Post Office Distribution in the US",
+       caption = 
+         "By @rppfeiffer|#TidyTuesday|Data:Cameron Blevins & Richard W. Helbock") 
+plot  
 
-
+ggsave("Week162021.png",plot = plot, height = 5, width = 7, units = 'in')
